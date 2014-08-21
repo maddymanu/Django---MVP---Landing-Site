af4817373e8cfc03c7e2c0c40a7e62394f71ec8c
Aditya Bansal <maddymanu@hotmail.com>
2014-08-21 10:27:47 +0530

    SignUp form works.

--- mvp_landing/settings.py
+++ mvp_landing/settings.py
@@ -82,3 +82,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+# Templates location
+TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(BASE_DIR) , "static" , "templates"),
+)
